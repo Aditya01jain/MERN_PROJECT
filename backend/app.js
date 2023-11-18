50fee03e279d0155d1acc4bd5ca5db36bc29a0dc
Aditya01jain <118585917+Aditya01jain@users.noreply.github.com>
2023-11-18 17:00:20 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -27,9 +27,6 @@ mongoose.connect(process.env.DATABASE, {
   .catch((err) => console.log(err));
 
 // MIDDLEWARE
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
-}
 app.use(bodyParser.json({ limit: "5mb" }));
 app.use(bodyParser.urlencoded({
   limit: "5mb",
@@ -46,17 +43,9 @@ app.use('/api', jobRoute);
 
 __dirname = path.resolve()
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  )
-} else {
-  app.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send('API is running....')
   })
-}
 
 // error middleware
 app.use(errorHandler);
